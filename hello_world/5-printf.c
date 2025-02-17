dd293fbf537d3ad0445a72ce03c0234bd0420f79
GuarickGit <10615@holbertonstudents.com>
2025-02-17 13:29:52 +0000

    Printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,8 +4,9 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }

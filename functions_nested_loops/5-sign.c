1081043fed8682ba098c8cbcff1372a5b74760e2
GuarickGit <10615@holbertonstudents.com>
2025-02-21 12:55:18 +0000

    add 5-sign.c and update main.h

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *
+ * @n: check n
+ *
+ * Return: if "n" is greater than zero, return 1 and prints +
+ * if "n" is zero, return 0 and prints 0
+ * if "n" is less than zero, return -1 and prints -
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

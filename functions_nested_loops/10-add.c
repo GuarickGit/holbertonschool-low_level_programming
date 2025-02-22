afb83e8362cd02028eb4bea3ff92dec9303e87b2
GuarickGit <10615@holbertonstudents.com>
2025-02-22 11:45:38 +0000

    add 10-add.c and update main.h

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * @number1: First integer
+ *
+ * @number2: second integer
+ *X
+ * Return: Result of number1 + number2
+ */
+
+int add(int number1, int number2)
+{
+	return (number1 + number2);
+}

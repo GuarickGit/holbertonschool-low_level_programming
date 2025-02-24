832046aabe8f26c361ca8618f8793dbbfd4bbfca
GuarickGit <10615@holbertonstudents.com>
2025-02-24 12:37:57 +0000

    add 2-mul.c and update main.h

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - mutiplies two integers
+ *
+ * @a: First number to multiply
+ *
+ * @b: second number to mutiply
+ *
+ * Return: product of a*b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

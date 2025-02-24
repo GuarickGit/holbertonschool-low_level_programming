9deb92670086b33d1b503dd4b8b273904b074082
GuarickGit <10615@holbertonstudents.com>
2025-02-24 12:53:21 +0000

    add 3-print_numbers and update main.h

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9.
+ */
+
+void print_numbers(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers <= 9; numbers++)
+	{
+		_putchar(numbers + '0');
+	}
+	{
+		_putchar('\n');
+	}
+}

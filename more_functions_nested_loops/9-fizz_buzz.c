8c5943b59cfe7524762bd5c5dd06a49e2b5dcd8a
GuarickGit <10615@holbertonstudents.com>
2025-02-25 10:28:03 +0000

    add 9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+			printf("Fizz");
+		if (n % 5 == 0)
+			printf("Buzz");
+		if (n % 3 != 0 && n % 5 != 0)
+			printf("%d", n);
+		if (n < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}

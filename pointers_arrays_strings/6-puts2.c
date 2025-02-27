813d1614a521bf86f55a93d02a34894a8d078d39
GuarickGit <10615@holbertonstudents.com>
2025-02-27 14:16:25 +0000

    update 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,11 @@ void puts2(char *str)
 {
 	int i;
 
-	/*init i = 0, while str[i] != \o , incremente de 2 */
-	for (i = 0; str[i] != '\0'; i += 2)
+	/*init i = 0, while str[i] != \o , incremente de 1 */
+	for (i = 0; str[i] != '\0'; i++)
 	{
+		/* si i % 2 = 0, alors mutliple de 2, donc 1 chara sur 2  */
+		if (i % 2 == 0);
 		_putchar(str[i]);
 	}
 	_putchar('\n');

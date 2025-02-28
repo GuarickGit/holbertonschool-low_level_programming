e4c05e898bcab91c6b2dba1ad595a0c800c3add1
GuarickGit <10615@holbertonstudents.com>
2025-02-28 18:15:03 +0100

    update strcat.c (parentheses on return)

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,20 +11,17 @@ char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
 
-	/* Find the end of dest */
+	/* cherche la fin de dest */
 	while (dest[i] != '\0')
 		i++;
 
-	/* Append src to dest */
+	/* ajouter src a dest */
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
-
-	/* Add null terminator */
 	dest[i] = '\0';
 
-	return dest;
 }

391ae7fe389b05247650f5d03c55c7ce554e96f5
GuarickGit <10615@holbertonstudents.com>
2025-03-03 13:40:20 +0100

    update 2-strchr.c (restart from 0)

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,23 @@
 #include "main.h"
 
 /**
- * _strchr - locates a character in a string
- * @s: the string to search in
- * @c: the character to locate
+ * _strchr - locates character in a string
  *
- * Return: pointer to the first occurrence of c in s, or NULL if not found
+ * @s: pointer of string source
+ * @c: character to locate
+ *
+ * Return: return s with character locate
  */
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++) /* parcours la chaine jusqu'à '\0' */
+	while (*s++) /* boucle tant qu'on n'atteint pas '\0' */
 	{
-		if (s[i] == c) /* si on trouve le caractère */
-			return (&s[i]); /* retourne un pointeur vers ce caractère */
+		if (*s == c) /* si le caractère actuel correspond à c */
+		{
+			return (s); /* retourne l'adresse du premier c trouvé */
+		}
+		s++;
 	}
-
-	/* vérification si le caractère est '\0' */
-	if (c == '\0')
-		return (&s[i]); /* retourne le pointeur sur le caractère nul */
+	return (0); /* retourne NULL si c n'est pas trouvé dans la chaine */
 }

4438f5bc28389b864798eaf08f38b4387933e1b3
GuarickGit <10615@holbertonstudents.com>
2025-03-05 09:24:24 +0100

    add 7-print_chessboard and update main.h

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int column;
+
+	for (row = 0; row < 8; row++) /* parcours les lignes */
+	{
+		for (column = 0; column < 8; column++) /* parcours les colonnes */
+		{
+			_putchar(a[row][column]); /* affiche le caractère stocké */
+		}
+		_putchar('\n');
+	}
+}

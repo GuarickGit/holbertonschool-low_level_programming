0978118fef75e0a15caecabaeb703345bd49563b
GuarickGit <10615@holbertonstudents.com>
2025-03-06 15:03:44 +0100

    update 0-whatsmyname.c (Missing description)

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - prints the program name followed by a new line
+ *
+ * @argc: the number of arguments passed to the program (unused)
+ * @argv: an array of strings containing the arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);

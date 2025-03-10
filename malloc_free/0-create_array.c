f4a3ac46ed5530599cf5b240bbfba07e9a336043
GuarickGit <10615@holbertonstudents.com>
2025-03-10 10:47:01 +0100

    update 0-create_array.c (add comments)

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,18 +15,23 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *arr;
 
+	/* vérifie si la taille demandée est 0, retourne NULL dans ce cas */
 	if (size == 0)
 		return (NULL);
 
+	/*alloue dynamiquement de la mémoire pour le tableau de caractères */
 	arr = malloc(size * sizeof(char));
 
+	/* vérifie si l'allocation mémoire a échoué */
 	if (!arr)
 		return (NULL);
 
+	/* initialise chaque élément du tableau avec le caractère spécifié */
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
 
+	/* retourne le pointeur vers le tableau alloué et initialisé */
 	return (arr);
 }

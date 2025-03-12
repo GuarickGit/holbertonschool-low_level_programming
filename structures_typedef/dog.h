baa1b8d9ff14f2fe9cd318c20ac618b0d7907da0
GuarickGit <10615@holbertonstudents.com>
2025-03-12 15:29:11 +0100

    add 1-init_dog and update dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,4 +15,8 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
+/*-------------------------------------------------*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/*-------------------------------------------------*/
 #endif /* DOG_H */

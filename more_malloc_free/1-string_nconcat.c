0cfe0efed4ad3d336c8587eba390bb95c82b4f5d
GuarickGit <10615@holbertonstudents.com>
2025-03-12 10:10:35 +0100

    update 1-string_nconcat.c(more than 40 lines)

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -17,31 +17,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
 		;
-
 	if (n < j)
 	{
 		s3 = malloc((i + n + 1) * sizeof(char));
-
 		if (!s3)
 		return (NULL);
-
 		for (k = 0; k < i; k++)
 			s3[k] = s1[k];
-
 		for (k = 0; k < n; k++)
 			s3[i + k] = s2[k];
-
 		s3[i + n] = '\0';
 	}
-
 	else
 	{
 		s3 = malloc((i + j + 1) * sizeof(char));
@@ -57,6 +49,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 		s3[i + j] = '\0';
 	}
-
 	return (s3);
 }

0e0b195fbf348016c994ad1c691faba59df72d7e
GuarickGit <10615@holbertonstudents.com>
2025-03-12 13:15:03 +0100

    add 3-array_range.c and update main.h

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * array_range - create an array of integers
+ *
+ * @min: minimal value
+ * @max: maximum value
+ *
+ * Return: pointer to the array or NULL if fail
+ */
+
+int *array_range(int min, int max)
+{
+	int i;
+	int *arr;
+
+	/* vérifie si min est plus grand que max */
+	if (min > max)
+		return (NULL);
+
+	/* alloue la mémoire pour le tableau */
+	arr = malloc((max - min + 1) * sizeof(int));
+
+	if (!arr)
+		return (NULL);
+
+	/* remplit le tableau avec les valeurs de min à max */
+	for (i = 0; min <= max; i++)
+	{
+		arr[i] = min;
+		min++;
+	}
+
+	/* retourne le tableau alloué */
+	return (arr);
+}

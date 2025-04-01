b3f80f1bbf820322639f123a91f5dc7680718f08
GuarickGit <10615@holbertonstudents.com>
2025-04-01 13:22:47 +0200

    update 3-add_node_end.c (betty)

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -17,7 +17,7 @@ unsigned int _strlen(const char *s)
 	return (i);
 }
 
- /**
+/**
  * add_node_end - Adds a new node at the end of a list_t list
  * @head: Double pointer to the head of the list
  * @str: String to duplicate and store in the new node

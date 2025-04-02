2d79bf413c24b13d5d19229efc873595f6b8e2ed
GuarickGit <10615@holbertonstudents.com>
2025-04-02 15:34:12 +0200

    add 4-free_dlistint.c and update lists.h

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp; /* Pointeur temporaire pour parcourir la liste */
+	dlistint_t *next; /* Pointeur vers le nœud suivant */
+
+	tmp = head; /* Initialisation du parcours de la liste à partir de head */
+
+	while (tmp != NULL) /* Tant qu'on n'est pas à la fin de la liste */
+	{
+		next = tmp->next; /* Sauvegarde du nœud suivant */
+		free(tmp); /* Libération du nœud lui-même */
+		tmp = next; /* Passage au nœud suivant */
+	}
+}
